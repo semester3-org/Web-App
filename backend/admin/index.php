83a8dfc3b50a3d78e063875f7430e88c4fa0bf74
iqbaalmln10 <r.iqbal.maulana.ibrahim@gmail.com>
2025-09-08 21:23:49 +0700

    Menambah class untuk template struktur

---
+++ backend/admin/index.php
@@ -0,0 +1,4 @@
+<?php
+// Hanya Contoh Template Saja
+echo "<h1>Admin Backend - Under Development</h1>";
+?>
---
+++ backend/user/index.php
@@ -0,0 +1,4 @@
+<?php
+// Hanya Contoh Template Saja
+echo "<h1>User Backend - Under Development</h1>";
+?>
