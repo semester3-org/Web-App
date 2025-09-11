c5eb58c46f8fafb39a33d32639594855be79f546
aryabintangj <richbuaya@gmail.com>
2025-09-11 12:24:21 +0700

    menambahkan struktur

--- backend/admin/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Hanya Contoh Template Saja
-echo "<h1>Admin Backend - Under Development</h1>";
-?>
--- backend/user/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Hanya Contoh Template Saja
-echo "<h1>User Backend - Under Development</h1>";
-?>
